2868986a4d9553cfd5ec911727e78208b58c5132
newt0ns <nate.stuart@gmail.com>
2019-03-02 17:49:36 -0800

    Update and rename unlock.py to lock.py

---
+++ lock.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+'''farmware-unlock
+'''
+
+from farmware_tools import device
+device.emergency_lock()
+device.log(message='farware-lock exectued", message_type='success')
--- unlock.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-'''farmware-unlock
-'''
-
-from farmware_tools import device
-device.emergency_unlock()
-device.log(message='farware-unlock exectued", message_type='success')
